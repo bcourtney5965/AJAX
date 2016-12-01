d7822adef0b301fcf7b98825582b842c8ec3c541
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-12-01 11:44:18 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+// var path = require('path');
+var app = express()
+
+// app.get('*', function(req, res) {
+//   res.sendFile(path.resolve(__dirname, 'client/public/index.html'));
+// });
+
+app.use(express.static('client/public'));
+
+app.get('/test', function (req, res) {
+  console.log('into the router');
+  res.send('From Server')
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
+
+
+// am I serving the right stuff? or
+// do I have to serve the bundle?
+
